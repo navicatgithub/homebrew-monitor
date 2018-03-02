e9503961807810b5a3ebaf846383bc83c9da88f1
Navicat <github@navicat.com>
2018-03-02 15:39:26 +0800

    Add navicatmonitor v1.0.0.1

---
+++ navicatmonitor.rb
@@ -0,0 +1,68 @@
+class Navicatmonitor < Formula
+  desc "Navicat Monitor is a safe, simple and agentless remote server monitoring tool that is packed with powerful features to make your monitoring effective as possible."
+  homepage "https://www.navicat.com/en/products#navicat-monitor"
+  url "http://download3.navicat.com/monitor-download/homebrew/navicat-monitor_1.0.0.1.tar.gz"
+  sha256 "bef88f5d92e663fbdc984d34739d5b908c571d44c6900c0af7097d5284b47428"
+
+  bottle :unneeded
+
+  depends_on "openssl"
+  depends_on "libyaml"
+  depends_on "libffi"
+  depends_on "readline"
+  depends_on "mysql"
+  depends_on "sqlite"
+  depends_on "postgresql"
+
+  def install
+    # Install ruby gems
+    system "./install.sh"
+    libexec.install Dir["*", ".ruby"]
+
+    # Symlink var and tmp to persist across version update
+    rm_rf "#{libexec}/var"
+    mkdir_p "#{var}/navicatmonitor/var"
+    ln_s "#{var}/navicatmonitor/var", "#{libexec}/var"
+
+    rm_rf "#{libexec}/tmp"
+    mkdir_p "#{var}/navicatmonitor/tmp"
+    ln_s "#{var}/navicatmonitor/tmp", "#{libexec}/tmp"
+
+    # Create wrapper binary
+    bin.write_exec_script "#{libexec}/wrapper"
+    mv "#{bin}/wrapper", "#{bin}/navicatmonitor"
+  end
+
+  plist_options :startup => "true"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <dict>
+        <key>Crashed</key>
+        <true/>
+      </dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{libexec}/wrapper</string>
+        <string>start</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{libexec}</string>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/navicatmonitor version"
+  end
+end
+
